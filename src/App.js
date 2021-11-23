4bed1e1d2cb92656e66ba18ec5d5aff1cafc8989
Calvin Putera Loka <calvinputeraloka@gmail.com>
2021-11-23 17:59:06 +0700

    make folder structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
 
+import { Brand, CTA, Navbar } from './components';
+import { Blog, Features, Footer, Header, Possibility, WhatGPT3 } from './containers';
+
+import './App.css';
+
 const App = () => {
     return (
-        <div>
-            <h1>GPT</h1>
+        <div className="App">
+            <div className="gradient_bg">
+                <Navbar />
+                <Header />
+            </div>
+            <Brand />
+            <WhatGPT3 />
+            <Features />
+            <Possibility />
+            <CTA />
+            <Blog />
+            <Footer />
         </div>
     );
 };
