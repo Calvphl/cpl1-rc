fcce0d0d5bfd9b14f93d1787e9490aef7fcfa73e
Calvin Putera Loka <calvinputeraloka@gmail.com>
2021-11-24 17:14:19 +0700

    add navbar responsive and navbar-menu

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
+DEPEDENCIES
+
+1. INSTALL npm install react-icons --save
+
+/////////////////////////////////////////////////////////////////////////////////
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
+import React from "react";
 
-import { Brand, CTA, Navbar } from './components';
-import { Blog, Features, Footer, Header, Possibility, WhatGPT3 } from './containers';
+import { Brand, CTA, Navbar } from "./components";
+import {
+  Blog,
+  Features,
+  Footer,
+  Header,
+  Possibility,
+  WhatGPT3,
+} from "./containers";
 
-import './App.css';
+import "./App.css";
 
 const App = () => {
-    return (
-        <div className="App">
-            <div className="gradient_bg">
-                <Navbar />
-                <Header />
-            </div>
-            <Brand />
-            <WhatGPT3 />
-            <Features />
-            <Possibility />
-            <CTA />
-            <Blog />
-            <Footer />
-        </div>
-    );
+  return (
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
