a438a9f7eab8324786020680469126c1a03399b3
Calvin Putera Loka <calvinputeraloka@gmail.com>
2021-12-03 16:27:29 +0700

    adding footer and finishing with deploy

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,8 +1,49 @@
 import React from "react";
 import "./footer.css";
+import gpt3Logo from "../../assets/logo.svg";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="gpt3__footer section__padding">
+      <div className="gpt3__footer-heading">
+        <h1 className="gradient__text">
+          Do you want to step in to the future before others
+        </h1>
+      </div>
+      <div className="gpt3__footer-btn">
+        <p>Request Early Access</p>
+      </div>
+
+      <div className="gpt3__footer-links">
+        <div className="gpt3__footer-links_logo">
+          <img src={gpt3Logo} alt="logo" />
+          <p>Milwauke 123. All Right Reserved</p>
+        </div>
+        <div className="gpt3__footer-links_div">
+          <h4>Links</h4>
+          <p>Overons</p>
+          <p>Social Media</p>
+          <p>Counters</p>
+          <p>Contact</p>
+        </div>
+        <div className="gpt3__footer-links_div">
+          <h4>Company</h4>
+          <p>Term & Conditions</p>
+          <p>Privacy Policy</p>
+          <p>Contact</p>
+        </div>
+        <div className="gpt3__footer-links_div">
+          <h4>Get In Touch</h4>
+          <p>Milwauke 123</p>
+          <p>08-123-123-1234</p>
+          <p>bla@blabla.com</p>
+        </div>
+      </div>
+      <div className="gpt3__footer-copyright">
+        <p>©2021 Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
